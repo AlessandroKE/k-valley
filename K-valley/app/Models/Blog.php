360e27b0ce9aa877b09a84b208926fb4085ddb46
SandroKE <koomealessandro@gmail.com>
2024-08-14 16:18:24 -0600

    create blog: model, factory and migration

---
+++ K-valley/app/Models/Blog.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'content',
+        'is_published',
+        'author_id'
+    ];
+
+    public function author(){
+        // To be create by 
+        return $this->belongsTo(Author::class, 'author_id', 'id');
+    }
+}
